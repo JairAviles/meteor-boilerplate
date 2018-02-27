13ca49348809c735e02bd683138ce39efbb5b054
Jair Avilés <hi@jairaviles.mx>
2018-02-27 09:53:10 -0600

    improve Router definition
    fix minor issues

--- client/main.js
+++ client/main.js
@@ -1,16 +1,12 @@
 import { Meteor } from 'meteor/meteor';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import { Tracker } from 'meteor/tracker';
 import { Session } from 'meteor/session';
 
-import { routes, onAuthChanges } from '../imports/routes/routes';
+import { AppRouter, onAuthChanges } from '../imports/routes/AppRouter';
 import './../imports/startup/simple-schema-configuration';
 
-Tracker.autorun(() => {
-    const isAuthenticated = !!Meteor.userId();
-    onAuthChanges(isAuthenticated);
-});
-
 Meteor.startup(() => {
-    ReactDOM.render(routes, document.getElementById('app'));
+    ReactDOM.render(<AppRouter />, document.getElementById('app'));
 });
